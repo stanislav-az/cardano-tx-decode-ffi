6e43228dd0710760c6a487a25c8a130c4a78bc08
Stanislav Zhdanovich <st.zhdanovich@gmail.com>
2022-10-24 16:08:22 +0700

    copy output string

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,6 +5,7 @@ use std::ffi::{c_char, CStr, CString};
 use std::io::Cursor;
 
 pub type Str = *const c_char;
+pub type StrOut = *mut c_char;
 
 fn deserialize_tx(tx_hex: &str) -> Result<Transaction, String> {
     let tx_bytes = hex::decode(tx_hex).map_err(|e| e.to_string())?;
@@ -15,7 +16,7 @@ fn deserialize_tx(tx_hex: &str) -> Result<Transaction, String> {
 
 // TODO return empty instead of panic?
 #[no_mangle]
-pub extern "C" fn tx_cbor_to_json(ptr: Str) -> Str {
+pub extern "C" fn tx_cbor_to_json(ptr: Str, out: StrOut) {
     unsafe {
         let cstring = CStr::from_ptr(ptr);
         if let Ok(tx_hex) = cstring.to_str() {
@@ -25,11 +26,10 @@ pub extern "C" fn tx_cbor_to_json(ptr: Str) -> Str {
                     Ok(Err(json_err)) => json_err.to_string(),
                     Ok(Ok(res)) => res,
                 };
-            let res = CString::new(json);
-            match res {
-                Err(_) => panic!("Unable to convert output"),
-                Ok(s) => s.as_ptr(),
-            }
+            let res = CString::new(json).expect("CString::new failed");
+            let count = res.to_bytes_with_nul().len();
+            let raw = res.into_raw();
+            raw.copy_to(out, count); 
         } else {
             panic!("Unable to convert input");
         }
