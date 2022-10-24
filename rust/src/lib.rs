a9659c6b396dcb2a7431f096446fdc43d80cb6ce
Stanislav Zhdanovich <st.zhdanovich@gmail.com>
2022-10-24 13:39:17 +0700

    returt cstring from rust

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,10 @@
 use cardano_serialization_lib::utils::Deserialize;
 use cardano_serialization_lib::Transaction;
 use cbor_event::de::*;
-use std::ffi::{c_char, CStr};
+use std::ffi::{c_char, CStr, CString};
 use std::io::Cursor;
+
+pub type Str = *const c_char;
 
 fn deserialize_tx(tx_hex: &str) -> Result<Transaction, String> {
     let tx_bytes = hex::decode(tx_hex).map_err(|e| e.to_string())?;
@@ -11,16 +13,22 @@ fn deserialize_tx(tx_hex: &str) -> Result<Transaction, String> {
     tx.map_err(|e| e.to_string())
 }
 
+// TODO return empty instead of panic?
 #[no_mangle]
-pub extern "C" fn tx_cbor_to_json(ptr: *const c_char) -> String {
+pub extern "C" fn tx_cbor_to_json(ptr: Str) -> Str {
     unsafe {
         let cstring = CStr::from_ptr(ptr);
         if let Ok(tx_hex) = cstring.to_str() {
             let tx = deserialize_tx(tx_hex);
-            match tx.map(|t| t.to_json()) {
-                Err(e) => e,
-                Ok(Err(json_err)) => json_err.to_string(),
-                Ok(Ok(res)) => res,
+            let json = match tx.map(|t| t.to_json()) {
+                    Err(e) => e,
+                    Ok(Err(json_err)) => json_err.to_string(),
+                    Ok(Ok(res)) => res,
+                };
+            let res = CString::new(json);
+            match res {
+                Err(_) => panic!("Unable to convert output"),
+                Ok(s) => s.as_ptr(),
             }
         } else {
             panic!("Unable to convert input");
