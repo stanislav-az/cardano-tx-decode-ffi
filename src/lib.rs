b95181e3e95e107aa4b9f7745716fbfdcbbac2d2
stanislav-az <st.zhdanovich@gmail.com>
2022-11-01 18:28:08 +0700

    use hs bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use cardano_serialization_lib::utils::Deserialize;
 use cardano_serialization_lib::Transaction;
 use cbor_event::de::*;
 use std::io::Cursor;
+use hs_bindgen::*;
 
 // This function converts hex encoded tx CBOR into cardano_serialization_lib::Transaction.
 fn deserialize_tx(tx_hex: &str) -> Result<Transaction, String> {
@@ -13,11 +14,14 @@ fn deserialize_tx(tx_hex: &str) -> Result<Transaction, String> {
 
 // This function converts hex encoded tx CBOR into JSON.
 // If any error occurred, it is converted to string and directed to out.
-pub fn tx_cbor_to_json(tx_hex: String) -> String {
+
+#[hs_bindgen]
+pub fn tx_cbor_to_json(tx_hex: String, mut out: String) {
     let tx = deserialize_tx(&tx_hex);
-    match tx.map(|t| t.to_json()) {
+    let res = match tx.map(|t| t.to_json()) {
         Err(e) => e,
         Ok(Err(json_err)) => json_err.to_string(),
         Ok(Ok(res)) => res,
-    }
+    };
+    out = res;
 }
